700104811339968a412bab63aba0f9356541d0eb
01001111 <01001111@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-18 10:59:21 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/scr0bbled/trunk@103166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== scr0bbled ===
+Contributors: 01001111
+Tags: widget, music, lastfm
+Requires at least: 2.0.2
+Tested up to: 2.7
+Stable tag: trunk
+
+Display the latest [available] album artwork from tracks recently scrobbled to LastFM.  LastFM API Key required.
+
+== Description ==
+
+The scr0bbled widget/plugin will allow you to display a variable (and approximate) list of album images from tracks recently scrobbled to LastFM for any specific user.
+
+* Note: A LastFM API key is required for this widget to work.  See http://www.last.fm/api for more information.
+
+== Installation ==
+
+
+1. Upload 'scr0bbled.php' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure the widget in the widget section.
+
+To use as a plugin in your theme, include the following:
+`<php	$s = new scr0bbled($apiKey,$user,$nAlbums,$imageSize,$title,$divid);
+	echo $s->latestAlbums(); ?>`
+
+And set the appropriate variables or leave blank for defaults ($apiKey and $user are required).
+
+The configuration parameters are:
+
+* Last.FM API Key: Your LastFM API key.
+* Last.FM User:	 The username of the LastFM account you wish to follow (ideally your own).
+* Number of Albums to Display: The number of albums the widget will attempt to display.
+* Image Size: The album image size (small, medium, large) the widget will try to grab.  Image sizes may vary and sometimes a low-res version is not available so be sure to do some image scaling in your CSS styling.
+* Title: The title of this section.
+* Wrapper Div ID: The id for the widget's wrapper div for your CSS styling convenience.  Leave blank to omit the div wrapper entirely.
+
+
+== Frequently Asked Questions ==
+
+= Why do I need a LastFM API key? =
+
+Because it is required to use the LastFM API and you can't have mine.
+
+= I just listened to / scrobbled a song and the image isn't showing up.  Why? =
+
+If there is no album art associated with that track on LastFM then it will not be returned to the widget.  Submit the appropriate artwork to LastFM to make it available to the widget and the world over.
+
+= Less than X albums are showing up when I've clearly selected X =
+
+There's no way to grab a certain number of recently listened albums in one request so, rather than assaulting LastFM with multiple requests, I've used 10 as a multiplier to approximate the tracks needed to get the requested number of albums.  Tracks without artwork or albums with a large number of tracks (like Pg Destroyer's "37 Counts of Battery") can thwart this valiant effort.
+
+== Screenshots ==
+
+None at the moment.
---
+++ scr0bbled.php
@@ -0,0 +1,258 @@
+<?php
+/*
+Plugin Name: scr0bbled
+Plugin URI: http://wordpress.org/#
+Description: latest [available] album artwork from recently scrobbled tracks
+Author: Oliver C Dodd
+Version: 1.0
+Author URI: http://01001111.net
+  
+  Copyright (c) 2009 Oliver C Dodd - http://01001111.net
+  
+  Much of the functionality is taken from the free 01001111 library
+  
+  *NOTE: you will need to apply for and supply your own Last.FM API Key
+  
+  Permission is hereby granted,free of charge,to any person obtaining a 
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction,including without limitation
+  the rights to use,copy,modify,merge,publish,distribute,sublicense,
+  and/or sell copies of the Software,and to permit persons to whom the 
+  Software is furnished to do so,subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS OR
+  IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER
+  LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,ARISING
+  FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+*/
+class scr0bbled
+{
+	/*-VARIABLES----------------------------------------------------------*/
+	private $apiKey;
+	private $user;
+	private $nAlbums;
+	private $imageSize;
+	public $title;
+	private $divid;
+	
+	public static $imageSizes = array('small','medium','large');
+	
+	/*-CONSTRUCT----------------------------------------------------------*/
+	//public function __construct($k,$u,$n=10,$s='medium',$t="scr0bbled")
+	public function scr0bbled($k,$u,$n=10,$s='medium',$t="scr0bbled",$d="")
+	{
+		$this->apiKey		= $k;
+		$this->user		= $u;
+		$this->nAlbums		= $n;
+		$this->imageSize	= $s;
+		$this->divid		= $d;
+	}
+	
+	/*-URL----------------------------------------------------------------*/
+	private function url($method,$args=array())
+	{
+		$args = is_array($args) ? http_build_query($args) : $args;
+		if ($args) $args = "&$args";
+		return "http://ws.audioscrobbler.com/2.0/".
+			"?method=$method&api_key=$this->apiKey$args";
+	}
+	/*-GET RECENT TRACKS--------------------------------------------------*/
+	private function recentTracks($limit=50)
+	{
+		$url = $this->url("user.getrecenttracks",
+			"user=$this->user&limit=$limit");
+		$recentTracksXML = @file_get_contents($url);
+		return $this->parseRecentTracks($recentTracksXML);
+	}
+	/*-PARSE RECENT TRACKS------------------------------------------------*/
+	private function parseRecentTracks($xml)
+	{
+		$doc = new DOMDocument();
+		if (!$xml||!$doc->loadXML($xml)) return array();
+		
+		$tracks = array();
+		foreach ($doc->getElementsByTagName("track") as $node) {
+			$tracks[] = array(
+				'track'		=> self::tagValue($node,'name'),
+				'artist'	=> self::tagValue($node,'artist'),
+				'album'		=> self::tagValue($node,'album'),
+				'image'		=> self::tagValue($node,'image',
+							array("size"=>$this->imageSize))
+			);
+		}
+		return $tracks;
+	}
+	/*-XML PARSING SPECIFICS----------------------------------------------*/
+	public static function tagValue($node,$tag,$attributes=array(),$valueIfNoChild=false)
+	{
+		if (!$attributes) {
+			$children = $node->getElementsByTagName($tag);
+			return $children->length
+				? $children->item(0)->nodeValue
+				: ($valueIfNoChild ? $node->nodeValue : "");
+		}
+		//get tags
+		$tags = $node->getElementsByTagName($tag);
+		//check attributes
+		$element = false;
+		for ($i = 0; $i < $tags->length; $i++) {
+			$found = true;
+			foreach ($attributes as $k => $v)
+				$found &= strcasecmp($tags->item($i)->getAttribute($k),$v) == 0;
+			if ($found) {
+				$element = $tags->item($i);
+				break;
+			}
+		}
+		return $element ? $element->nodeValue : "";
+	}
+	/*-GET LATEST ALBUMS--------------------------------------------------*/
+	public function latestAlbums()
+	{
+		if (!$this->apiKey||!$this->user) return "";
+		$tracks = $this->recentTracks($this->nAlbums*10);
+		
+		$html = "";
+		$albums = array();
+		foreach($tracks as $track) {
+			if (count($albums) >= $this->nAlbums) break;
+			if (!$track['image']) continue;
+			
+			$artist		= self::a($track,'artist');
+			$album		= self::a($track,'album');
+			$src		= self::a($track,'image');
+			
+			$title = "$artist - $album";
+			
+			if (isset($albums[$title])) continue;
+			$albums[$title] = $src;
+			
+			$html .= "<img	class='album'
+					title='".str_replace("'","&#39;",$title)."'
+					src='$src' /> ";
+		}
+		return $this->divid ? "<div id='$this->divid'>$html</div>" : $html;
+	}
+	
+	/*-ARRAY ACCESSOR-----------------------------------------------------*/
+	private static function a($a,$k,$d="")
+	{
+		if (!is_array($a)) return $d;
+		if (!isset($a[$k])) {
+			if ($set) $a[$k] = $d;
+			return $d; }
+		return $a[$k];
+	}
+	/*-GET OPTIONS--------------------------------------------------------*/
+	public static function getOptions()
+	{
+		return !($options = get_option('scr0bbled'))
+			? $options = array(
+				'apiKey'	=> "",
+				'user'		=> "",
+				'nAlbums'	=> 10,
+				'imageSize'	=> "medium",
+				'title'		=> "scr0bbled",
+				'divid'		=> "divid")
+			: $options;
+	}
+	
+	/*-MAKE OPTIONS-------------------------------------------------------*/
+	public static function makeOptions($a,$s="")
+	{
+		$options = "";
+		foreach ($a as $o) {
+			$sel = $o == $s ? " selected='selected' " : "";
+			$options .= "<option$sel>$o</o>";
+		}
+		return $options;
+	}
+}
+/*-OPTIONS--------------------------------------------------------------------*/
+function widget_scr0bbled_options()
+{
+	$options = scr0bbled::getOptions();
+	if($_POST['scr0bbled-submit'])
+	{
+		$options = array(	'apiKey'	=> $_POST['scr0bbled-apiKey'],
+					'user'		=> $_POST['scr0bbled-user'],
+					'nAlbums'	=> $_POST['scr0bbled-nAlbums'],
+					'imageSize'	=> $_POST['scr0bbled-imageSize'],
+					'title'		=> $_POST['scr0bbled-title'],
+					'divid'		=> $_POST['scr0bbled-divid']);
+		update_option('scr0bbled',$options);
+	}
+	?>
+	<p>	Last.FM API Key:
+		<input	type="text"
+			name="scr0bbled-apiKey"
+			id="scr0bbled-apiKey"
+			value="<?php echo $options['apiKey']; ?>"  />
+	</p>
+	<p>	Last.FM User:
+		<input	type="text"
+			name="scr0bbled-user"
+			id="scr0bbled-user"
+			value="<?php echo $options['user']; ?>"  />
+	</p>
+	<p>	Number of Albums to Display:
+		<select	name="scr0bbled-nAlbums"
+			id="scr0bbled-nAlbums">
+			<?php echo scr0bbled::makeOptions(range(1,20),
+				$options['nAlbums']); ?>
+		</select>
+	</p>
+	<p>	Image Size:
+		<select	name="scr0bbled-imageSize"
+			id="scr0bbled-imageSize">
+			<?php echo scr0bbled::makeOptions(scr0bbled::$imageSizes,
+				$options['imageSize']); ?>
+		</select>
+	</p>
+	<p>	Title:
+		<input	type="text"
+			name="scr0bbled-title"
+			id="scr0bbled-title"
+			value="<?php echo $options['title']; ?>"  />
+	</p>
+	<p>	Wrapper Div ID (blank for no div):
+		<input	type="text"
+			name="scr0bbled-divid"
+			id="scr0bbled-divid"
+			value="<?php echo $options['divid']; ?>"  />
+	</p>
+	<input type="hidden" id="scr0bbled-submit" name="scr0bbled-submit" value="1" />
+	<?php
+}
+/*-WIDGETIZE------------------------------------------------------------------*/
+function widget_scr0bbled_init()
+{
+	if (!function_exists('register_sidebar_widget')) { return; }
+	function widget_scr0bbled($args)
+	{
+		extract($args);
+		$options = scr0bbled::getOptions();
+		$s = new scr0bbled(	$options['apiKey'],
+					$options['user'],
+					$options['nAlbums'],
+					$options['imageSize'],
+					$options['title'],
+					$options['divid']);
+		echo "	$before_widget
+			$before_title $s->title $after_title
+				{$s->latestAlbums()}
+			$after_widget
+		";
+	}
+	register_sidebar_widget('scr0bbled','widget_scr0bbled');
+	register_widget_control('scr0bbled','widget_scr0bbled_options');
+}
+//add_action('init',"widget_scr0bbled_init");
+add_action('plugins_loaded', 'widget_scr0bbled_init');
+?>
